bbef50e9577f6fdf4b9e342982910df6f8adb754
Marahimimv <112489559+Marahimimv@users.noreply.github.com>
2022-11-22 20:41:38 +0400

    Add files via upload

---
+++ 11.21.2022/front/app.js
@@ -0,0 +1,16 @@
+// TASK1
+
+// function myFunction() {
+//   let age = prompt("Please enter your age", "18");
+//   console.log(age)
+//   if (age < 18) {
+//     document.getElementById("welcome").innerHTML =
+//     age + "! age too young";
+//   }
+// }
+
+TASK2
+function myFunction() {
+    confirm("Press a button!");
+    
+  }
